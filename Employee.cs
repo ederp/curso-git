6b1157ed84a42ccc54aec9ae535f7acfc66197bc
Eder <eder@directdigital.com.br>
2020-03-23 16:12:35 -0300

    Projeto que mostra o salário de alguns empregados de uma empresa

---
+++ Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secao6Listas {
+    class Employee {
+        public int id { get; private set; }
+        public string name { get; set; }
+        public double salary { get; set; }
+
+        public int getId() {
+            return id;
+        }
+
+        public string getName() {
+            return name;
+        }
+
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(double _salary) {
+            salary = _salary;
+        }
+
+        public void setId(int _id) {
+            id = _id;
+        }
+
+        public void setName(string _name) {
+            name = _name;
+        }
+
+        public void increaseSalary(double percentage) {
+            salary *= 1 + (percentage / 100);
+        }
+
+        public override string ToString() {
+            return getId() + ", " + getName() + ", " + getSalary().ToString("F2");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Secao6Listas {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("How many employees will be registered? ");
+            int qtd = int.Parse(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 1; i <= qtd; i++) {
+                Employee e = new Employee();
+                Console.WriteLine("Employee #{0}:", i);
+                Console.Write("Id: ");
+                e.setId(int.Parse(Console.ReadLine()));
+                Console.Write("Name: ");
+                e.setName(Console.ReadLine());
+                Console.Write("Salary: ");
+                e.setSalary(double.Parse(Console.ReadLine()));
+
+                employees.Add(e);
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int id = int.Parse(Console.ReadLine());
+            Employee emp = employees.Find(x => x.getId() == id);
+            if (emp == null) {
+                Console.WriteLine("This id does not exist!");
+            }
+            else {
+                Console.Write("Enter the percentage: ");
+                double percent = double.Parse(Console.ReadLine());
+                emp.increaseSalary(percent);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee obj in employees) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
